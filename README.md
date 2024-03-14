d89b6242160f24e333b3a8987cd3d9a4c81930b1
Egor Gavrilov <gavrilovegor519@gmail.com>
2024-03-14 22:19:24 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# coffeemachine
+
+This is a coffee machine REST service for remote control.
+
+This is a test task for AISA (https://aisa.ru/) company.
+
+It's based on Spring Boot 3 (with Spring 6), with Web, Data JPA
+and Security profile. Also, I'm using Flyway DB migration,
+Lombok, and some additional libraries.
---
+++ src/main/java/com/egor/coffeemachine/CoffeemachineApplication.java
@@ -0,0 +1,13 @@
+package com.egor.coffeemachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeemachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeemachineApplication.class, args);
+	}
+
+}
