12033b169e0652b403ce03108f50272fef31963b
Egor Gavrilov <gavrilovegor519@gmail.com>
2024-03-16 22:03:59 +0300

    Wrote a JavaDoc for coffee machine service layer.

--- src/main/java/com/egor/coffeemachine/service/CoffeeMachineService.java
+++ src/main/java/com/egor/coffeemachine/service/CoffeeMachineService.java
@@ -4,13 +4,65 @@ import com.egor.coffeemachine.dto.CoffeeMachineDto;
 
 import java.util.List;
 
+/**
+ * Coffee machine service layer.
+ * @author Egor Gavrilov (gavrilovegor519@gmail.com)
+ */
 public interface CoffeeMachineService {
+    /**
+     * Add a new coffee machine.
+     * @param coffeeMachineDto Coffee machine data
+     * @param username Username
+     */
     void addCoffeeMachine(CoffeeMachineDto coffeeMachineDto, String username);
+
+    /**
+     * On a coffee machine.
+     * @param machine Machine name
+     * @param username Username
+     */
     void on(String machine, String username);
+
+    /**
+     * Off a coffee machine.
+     * @param machine Machine name
+     * @param username Username
+     */
     void setBusy(String machine, String username);
+
+    /**
+     * Unset busy state of a coffee machine.
+     * @param machine Machine name
+     * @param username Username
+     */
     void unsetBusy(String machine, String username);
+
+    /**
+     * Get the states of a coffee machine.
+     * @param machine Machine name
+     * @param username Username
+     * @return List of states of a coffee machine
+     */
     CoffeeMachineDto getState(String machine, String username);
+
+    /**
+     * Off a coffee machine.
+     * @param machine Machine name
+     * @param username Username
+     */
     void off(String machine, String username);
+
+    /**
+     * Get all coffee machines.
+     * @param username Username
+     * @return List of coffee machines
+     */
     List<CoffeeMachineDto> getAll(String username);
+
+    /**
+     * Delete a coffee machine.
+     * @param machine Machine name
+     * @param username Username
+     */
     void deleteMachine(String machine, String username);
 }
